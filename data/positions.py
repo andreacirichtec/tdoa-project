85bcc57ec574a58d52aedd0ae9cb115a2725ba89
andreaciric <andrea.ciric@htecgroup.com>
2022-08-12 11:20:47 +0200

    added demo, extract, positions and constellations
    added demo, extract, positions and constellations

---
+++ data/positions.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+# change these addresses and file names
+src_address = "C:\\Users\\Andrea\\Documents\\ETF\\Theatre Sound\\tdoa\\data\\main_dataset\\flight-dataset\\csv-data\\const4\\const4-trial7-tdoa2-manual3.csv"
+dst_address = "C:\\Users\\Andrea\\Documents\\ETF\\Theatre Sound\\tdoa\\data\\extracted_data\\const4\\const4-trial7-tdoa2-manual3-extracted.xlsx"
+
+# read data from the csv file
+read_data = pd.read_csv(src_address)
+#print(read_data.info())
+
+# delete all rows where ground truth position is not provided
+read_data = read_data[read_data["pose_x"].notna()]
+#print(read_data.info())
+
+# extract only needed columns
+read_data = read_data[["idA", "idB", "tdoa_meas", "pose_x", "pose_y", "pose_z"]]
+read_data["idA"] = pd.to_numeric(read_data["idA"], downcast='integer')
+read_data["idB"] = pd.to_numeric(read_data["idB"], downcast='integer')
+#print(read_data.info())
+#print(read_data)
+
+# write into a new excel file
+read_data.to_excel(dst_address, index=False)
